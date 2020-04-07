f56f33cebcd6a9b39bcd934ef6700e0c7be7fef4
caskerin <60627176+caskerin@users.noreply.github.com>
2020-04-07 01:20:08 -0500

    foraab

---
+++ forms/forAB/forAB.js
@@ -0,0 +1,7 @@
+var namesArray=["Bob", "Janet", "Tom", "Bob", "Randy", "Elizabeth", "Kim",  "Nancy"]; 
+
+var backwardsArray=namesArray.reverse();
+
+
+for (i=0; i<namesArray.length; i++)
+  console.log(`The names in reverse are as follows: ${backwardsArray}`)
